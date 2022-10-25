764f36dddb6e327d6c3ecb40cb2b57238488e4ea
ResulHalit <resulhalit@gmail.com>
2022-10-25 14:05:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
-function App() {
-  return (
+const App = () =>{
+  const APP_ID={};
+  const APP_KEY={};
+
+  const exampleReq = {/*url gelecek */}
+
+ const [recipes, setRecipes] = useState([]);
+ const [search, setSearch] = useState("");
+ const [query, setQuery] = useState("chicken");
+
+ useEffect(()=>{
+  getRecipes();
+ }, []);
+
+ const getRecipes = async() => {
+  const response = await fetch(
+    `https://api.edemam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  );
+  const data = await response.json();
+  console.log(data.hits);
+ };
+
+ const updateSearch = e => {
+  setSearch(e.target.value);
+  console.log(search);
+ };
+
+ const getSearch = e => {
+  e.preventdefault();
+  setQuery(search);
+ }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" placeholder="search" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
-}
+};
+
+
+
+
+
 
 export default App;
